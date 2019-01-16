8a1cf37581930be33c3394b6401a98ee4b7e9342
mccaffertycr <mccaffertycr@gmail.com>
2019-01-16 11:34:15 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PasswordStrengthMeter from "./pwStrengthMeter";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    password: ""
+  };
+
   render() {
+    const { password } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="meter">
+          <input
+            autoComplete="off"
+            type="password"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <PasswordStrengthMeter password={password} />
+        </div>
       </div>
     );
   }
